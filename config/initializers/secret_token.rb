f835d6e16643c48c3f4eb4d4ee1df5ea98f14698
Bonnie Kwong <sibilant.threads@gmail.com>
2013-02-11 22:28:54 -0800

    initial commit with main image and brief description

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poetry::Application.config.secret_token = 'b6a2b8795e65dfa966f433b416b1ff2b60d1d7d711ed94f0bdc0fe43762da97c5b9f13cb8899ae88bedec13b4dc2d439753f41997a42b59bb172568619f97b86'
