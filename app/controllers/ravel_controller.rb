0e6f6472975a0f644f281a82d005077fcfa9ffe9
Bonnie <sibilant.threads@gmail.com>
2015-07-03 11:17:40 -0700

    testing mobile version of Warp and Weft as separate view

--- app/controllers/ravel_controller.rb
+++ app/controllers/ravel_controller.rb
@@ -6,6 +6,21 @@ class RavelController < ApplicationController
   end
 
   def warp_and_weft
+    puts "The web browser is: #{detect_browser}"
 
+    if detect_browser == "mobile_application"
+      render warp_and_weft_mobile
+    end
+  end
+
+  private
+  MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+
+  def detect_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      return "mobile_application" if agent.match(m)
+    end
+    return "application"
   end
 end
